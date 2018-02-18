7d74ccd414f77ca9f66d8adfe87436f768dcabe5
kozaky <gomcam.julio@icloud.com>
2018-02-18 21:37:58 +0100

    A const () statement was used to avoid being using const in every line in the resources file

--- resources/resources.go
+++ resources/resources.go
@@ -1,13 +1,15 @@
 package resources
 
-//MainDatabaseAddress is the address to the main database
-const MainDatabaseAddress = "127.0.0.1:3306"
+const (
+	//MainDatabaseAddress is the address to the main database
+	MainDatabaseAddress = "127.0.0.1:3306"
 
-//MainDatabase is the name of the main databased consumed by this application
-const MainDatabase = "GODORAXDB"
+	//MainDatabase is the name of the main databased consumed by this application
+	MainDatabase = "GODORAXDB"
 
-//MainDatabasePassword is the password for our main database
-const MainDatabasePassword = "NEITHANJU49GC12"
+	//MainDatabasePassword is the password for our main database
+	MainDatabasePassword = "NEITHANJU49GC12"
 
-//MainDatabaseUser is the user for our main database
-const MainDatabaseUser = "root"
+	//MainDatabaseUser is the user for our main database
+	MainDatabaseUser = "root"
+)
