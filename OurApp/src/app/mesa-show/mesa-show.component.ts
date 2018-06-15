e71c982a01fe6cc7774f3af09fa05627d823686e
Manuel J. Bellido-Lama <mbellidolama@gmail.com>
2018-06-15 13:20:58 +0200

    commit

--- OurApp/src/app/mesa-show/mesa-show.component.ts
+++ OurApp/src/app/mesa-show/mesa-show.component.ts
@@ -21,6 +21,7 @@ export class MesaShowComponent implements OnInit {
     constructor( private location: Location, private route: ActivatedRoute, private DbService: DBService) { }
 
     ngOnInit() {
+      this.Products = [];
       this.route.paramMap.subscribe((params: ParamMap) => {
         const param1 = String(params.get('paramId'));
         this.DbService.getRelationsFromTableById(param1).subscribe( relations => {
@@ -60,6 +61,7 @@ export class MesaShowComponent implements OnInit {
     deleteRelation(pid) {
       const relation = {table: this.mesa, product: pid};
       this.DbService.deleteRelation(relation);
+      this.ngOnInit();
       delete this.Products[pid];
     }
 
