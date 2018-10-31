4141fadd4ea33af8f17d63773aff72f1e7dffb7c
Manuel J. Bellido-Lama <mbellidolama@gmail.com>
2018-10-31 15:26:54 +0100

    navigation bar fixed

--- OurApp/src/app/app.component.ts
+++ OurApp/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-
 export class AppComponent {
   title = 'app';
+  currentUrl: String;
+
+  constructor(private router: Router) {
+    router.events.subscribe((_: NavigationEnd) => (this.currentUrl = _.url));
+  }
 
   Searching() {
     console.log('hola');
+  }
+  condition() {
+    return true;
   }
 }
